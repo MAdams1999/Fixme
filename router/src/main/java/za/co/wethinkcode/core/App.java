030a7cb441c20a70d450c510643489e3ff9bd6e4
Marchall Adams <50703766+MAdams1999@users.noreply.github.com>
2020-11-10 12:14:09 +0200

    There was a simple typo that made an error

---
+++ router/src/main/java/za/co/wethinkcode/core/App.java
@@ -0,0 +1,34 @@
+package za.co.wethinkcode.core;
+
+import za.co.wethinkcode.model.FixMessageDatabase;
+import za.co.wethinkcode.model.Router;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.NoSuchElementException;
+
+/**
+ * Router App
+ *
+ */
+public class App {
+//  public FixMessageDatabase fixMessageDatabase;
+  static Router router;
+
+  public static void main(String[] args) {
+    try {
+      router = new Router();
+    }
+    catch (IOException | SQLException e)
+    {
+      System.out.println(router._td.viewMessage("Well shit something went wrong...","error"));
+    }
+    catch (NoSuchElementException e)
+    {
+      System.out.println(router._td.viewMessage("Broker | Market Disconnection !","error"));
+    }
+    catch (NullPointerException e) {
+			System.out.println("");
+    }
+  }
+}
